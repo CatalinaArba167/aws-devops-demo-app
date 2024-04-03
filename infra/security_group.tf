21947e9d37d4c675b4c597963fcab518f3259a34
CatalinaArba167 <139768671+CatalinaArba167@users.noreply.github.com>
2024-04-03 14:29:08 +0300

    security group changed second time

--- infra/security_group.tf
+++ infra/security_group.tf
@@ -37,20 +37,3 @@ resource "aws_security_group" "terraform_online_shop_backend_security_group" {
 }
 
 
-resource "aws_security_group" "terraform_online_shop_ecs_security_group" {
-  name        = "terraform_online_shop_ecs_security_group"
-  description = "Allows all inbound and outbound traffic"
-  vpc_id      = aws_vpc.terraform_vpc.id
-}
-
-resource "aws_vpc_security_group_ingress_rule" "ecs_sg_ingress_all_internal" {
-  security_group_id = aws_security_group.terraform_online_shop_ecs_security_group.id
-  cidr_ipv4         = aws_vpc.terraform_vpc.cidr_block
-  ip_protocol       = "-1"
-}
-
-resource "aws_vpc_security_group_egress_rule" "ecs_sg_egress_all" {
-  security_group_id = aws_security_group.terraform_online_shop_ecs_security_group.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
